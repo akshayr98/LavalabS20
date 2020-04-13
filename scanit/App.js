c4f8e085d806e4fc4078f17691d74abff38d6ce9
akshayr98 <akravish@usc.edu>
2020-04-12 18:44:08 -0700

    Initial page set up

--- scanit/App.js
+++ scanit/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import logo from './assets/logo.png'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} style={{ position: 'absolute', width: 235, height: 70, left: 70, top: 275}} resizeMode='contain' /> 
+
+
+
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={{ position: 'absolute',
+                justifyContent: 'center',
+                width: 290,
+                height: 50,
+                left: 42,
+                top: 523,
+                backgroundColor: '#5caebb'
+        }}>
+        <Text style={{ fontSize: 20, color: '#fff', textAlign: 'center', margin: 'auto' }}>Log in</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => alert('Bye, world!')}
+        style={{ position: 'absolute',
+                justifyContent: 'center',
+                width: 290,
+                height: 50,
+                left: 42,
+                top: 584,
+                backgroundColor: '#5caebb'
+        }}>
+        <Text style={{ fontSize: 20, color: '#fff', textAlign: 'center', margin: 'auto' }}>Sign up</Text>
+      </TouchableOpacity>
     </View>
+    
   );
 }
 

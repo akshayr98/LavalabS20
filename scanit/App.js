a0adfa306782ba6c6cc29d885043ff35861bd637
akshayr98 <akravish@usc.edu>
2020-04-15 15:37:09 -0700

    Navigation working, created components folder

--- scanit/App.js
+++ scanit/App.js
@@ -1,48 +1,26 @@
 import React from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import 'react-native-gesture-handler';
+import { StyleSheet, Text, View } from 'react-native';
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import FirstPage from './components/FirstPage'
+import LoginPage from './components/LoginPage'
 
-import logo from './assets/logo.png'; 
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
 const Stack = createStackNavigator();
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Image source={logo} style={{ position: 'absolute', width: 235, height: 70, left: 70, top: 275}} resizeMode='contain' /> 
-
-      <TouchableOpacity
-        onPress={() => alert('Hello, world!')}
-        style={{ position: 'absolute',
-              justifyContent: 'center',
-                width: 290,
-                height: 50,
-                left: 42,
-                top: 523,
-                backgroundColor: '#5caebb'
-        }}>
-        <Text style={{ fontSize: 20, color: '#fff', textAlign: 'center', margin: 'auto' }}>Log in</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        onPress={() => alert('Bye, world!')}
-        style={{ position: 'absolute',
-                justifyContent: 'center',
-                width: 290,
-                height: 50,
-                left: 42,
-                top: 584,
-                backgroundColor: '#5caebb'
-        }}>
-        <Text style={{ fontSize: 20, color: '#fff', textAlign: 'center', margin: 'auto' }}>Sign up</Text>
-      </TouchableOpacity>
-    </View>
-    
-  );
+function App() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="FirstPage">
+          <Stack.Screen name="FirstPage" component={FirstPage} />
+          <Stack.Screen name="LoginPage" component={LoginPage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
---
+++ scanit/components/FirstPage.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { TouchableOpacity, Image, StyleSheet, Text, View, Button } from 'react-native';
+
+import logo from '.././assets/logo.png'; 
+
+export default class FirstPage extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      <Image source={logo} style={{ position: 'absolute', width: 235, height: 70, left: 70, top: 275}} resizeMode='contain' /> 
+
+      <TouchableOpacity
+         onPress={
+          () => this.props.navigation.navigate( 'LoginPage', {
+            username: 'Bill'
+          } )
+        }
+        style={{ position: 'absolute',
+              justifyContent: 'center',
+                width: 290,
+                height: 50,
+                left: 42,
+                top: 523,
+                backgroundColor: '#5caebb'
+        }}>
+        <Text style={{ fontSize: 20, color: '#fff', textAlign: 'center', margin: 'auto' }}>Log in</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => alert('Bye, world!')}
+        style={{ position: 'absolute',
+                justifyContent: 'center',
+                width: 290,
+                height: 50,
+                left: 42,
+                top: 584,
+                backgroundColor: '#5caebb'
+        }}>
+        <Text style={{ fontSize: 20, color: '#fff', textAlign: 'center', margin: 'auto' }}>Sign up</Text>
+      </TouchableOpacity>
+    </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
